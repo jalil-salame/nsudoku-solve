e98d8d031f4803ac54754aae285707b0d7702bc4
Jalil David Salamé Messina <jalil.salame@tum.de>
2023-01-30 19:24:55 +0100

    feat: Added benchmarking script

---
+++ bench.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+fail() {
+    echo "$1"
+    return 1
+}
+
+git diff-index --quiet HEAD \
+    || fail 'Git Tree Dirty commit changes before benchmarking'
+
+commit="$(git rev-parse --short HEAD)"
+file="bench-$commit"
+
+[ -e "$file" ] && fail "$file exists refusing to benchmark again"
+
+hyperfine "cargo run --release -- test --sudoku '......2.1..4.3.............37.....8.6..2........5.....54....6......7..4...2..1...'" | tee "$file"
+echo "Benchmarked $commit"
